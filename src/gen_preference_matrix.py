c1f40b4086363ce252de68fff47a14873e27b0dc
Emmy Liu <emmy.liu.1998@gmail.com>
2020-10-29 11:58:40 -0400

    change dir structure and begin tests

---
+++ src/gen_preference_matrix.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass, field
+#from itertools import product, combinations
+import numpy as np
+
+def init_preference_matrix(num_actions: int, num_criteria: int) -> np.ndarray:
+    return np.zeros((num_actions, num_actions))
+
+@dataclass
+class PreferenceMatrix:
+    num_actions: int
+    num_criteria: int = 1
+
+    def __post_init__(self):
+        self.data = init_preference_matrix(self.num_actions, self.num_criteria)
+        self.curr_condorcet_winner = None
+        self.num_observations = np.array([0] * self.num_actions)
+
+    def reset_state(self) -> None:
+        self.data = np.zeros((self.num_actions, self.num_actions))
+        self.curr_condorcet_winner = None
+        self.num_observations = np.array([0] * self.num_actions)
+        
+    def set_matrix_explicit(self, matrix) -> None:
+        if not isinstance(matrix, np.ndarray) or matrix.shape != (self.num_actions, self.num_actions):
+            raise Exception("Matrix must be an ndarray with shape (num_actions, num_actions)")
+        self.data = matrix
+        self.curr_condorcet_winner = None
+        self.num_observations = np.array([np.sum(self.data[i, :]) + np.sum(self.data[i, :]) - self.data[i, i] for i in range(self.num_actions)])
+
+    def record_win(self, winner: int, loser: int) -> None:
+        if winner != self.curr_condorcet_winner: 
+            self.curr_condorcet_winner = None
+        self.data[winner, loser] += 1
+        self.num_observations[winner] += 1
+        self.num_observations[loser] += 1
+
+    def condorcet_winner(self) -> int:
+        if self.curr_condorcet_winner == None:
+            highest = 0
+            winner = None
+            for i in range(self.num_actions):
+                if sum(self.data[i, :]) > 0.5 * self.num_observations[i] and sum(self.data[i, :]) > highest:
+                    highest = sum(self.data[i, :])
+                    winner = i
+
+            if winner:
+                self.curr_condorcet_winner = winner
+                return winner
+            return -1
+                    
+        else:
+            return self.curr_condorcet_winner
+
+
+
+if __name__ == "__main__":
+    matrix = PreferenceMatrix(num_actions=2, num_criteria=1)
+    print(matrix.num_observations)
+
---
+++ tests/test_gen_preference_matrix.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pytest
+
+from src.gen_preference_matrix import PreferenceMatrix
+
+class TestPreferenceMatrix:
+    @classmethod
+    def setup_class(self):
+        self.pm = PreferenceMatrix(3)
+    
+    def test_reset_state(self):
+        self.pm.reset_state()
+        assert not np.any(self.pm.data)
+        assert not np.any(self.pm.num_observations)
+        assert self.pm.curr_condorcet_winner is None
+
+    def test_set_matrix_wrong_size(self):
+        wrong_matrix = np.zeros((4, 5))
+        with pytest.raises(Exception):
+            self.pm.set_matrix_explicit(wrong_matrix)
+    
+    def test_set_matrix_wrong_type(self):
+        wrong_matrix = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        with pytest.raises(Exception):
+            self.pm.set_matrix_explicit(wrong_matrix)
+
+    def test_record_win(self):
+        winner = 0
+        loser = 1
+        self.pm.record_win(winner, loser)
+        assert self.pm.data[0, 1] == 1
+        assert self.pm.num_observations[0] == 1
+        assert self.pm.num_observations[1] == 1
+    
+    def test_get_condorcet_winner_empty(self):
+        self.pm.reset_state()
+        assert self.pm.condorcet_winner() == -1
+    
+    def test_get_condorcet_winner_not_exists(self):
+        self.pm.reset_state()
+        no_winner = np.array([[0, 2, 1],
+        [1, 0, 2],
+        [2, 1, 0]])
+        self.pm.set_matrix_explicit(no_winner)
+        print(self.pm.num_observations)
+        assert self.pm.condorcet_winner() == -1
+        
+        
+
+
+
+    
+    
+
+
+
+
